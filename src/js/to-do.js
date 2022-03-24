1989d200d6836e2c910b403ec1933f2fec6b2838
jaredsanderson <jaredsa@byui.edu>
2022-03-24 10:48:22 -0600

    Todo updated

--- src/js/to-do.js
+++ src/js/to-do.js
@@ -1,15 +1,19 @@
+/*
 const input = document.getElementsByClassName('input-value')[0];
+const description = document.getElementsByClassName('desc-value')[0];
 const addButton = document.getElementsByClassName('add-btn')[0];
 
 addButton.addEventListener('click', () => {
-    let items = JSON.parse(localStorage.getItem('item'));
-    if (items === null) {
+    let title = JSON.parse(localStorage.getItem('title'));
+    let desc = JSON.parse(localStorage.getItem('description'));
+    if (title && description === null) {
         itemList = [];
     } else {
-        itemList = items;
+        itemList = title;
     }
-    itemList.push(input.value);
-    localStorage.setItem('item', JSON.stringify(itemList));
+    itemList.push(input.value, description.value);
+    localStorage.setItem('title', JSON.stringify(itemList), );
+    localStorage.setItem('description', JSON.stringify(itemList));
     displayTodoList();
 });
 
@@ -17,11 +21,12 @@ function displayTodoList() {
 
     let output = '';
     let showList = document.querySelector('.list-item');
-    let items = JSON.parse(localStorage.getItem('item'));
-    if (items === null) {
+    let title = JSON.parse(localStorage.getItem('title'));
+    let desc = JSON.parse(localStorage.getItem('description'));
+    if (title && description === null) {
         itemList = [];
     } else {
-        itemList = items;
+        itemList = title;
     }
     
     itemList.forEach((element, index) => {
@@ -37,8 +42,32 @@ function displayTodoList() {
 displayTodoList();
 
 function deleteItem(index) {
-    let items = JSON.parse(localStorage.getItem('item'));
+    let title = JSON.parse(localStorage.getItem('title'));
+    let desc = JSON.parse(localStorage.getItem('description');)
     itemList.splice(index, 1);
-    localStorage.setItem('item', JSON.stringify(itemList));
+    localStorage.setItem('title', JSON.stringify(itemList));
+    localStorage.setItem('description', JSON.stringify(itemList));
     displayTodoList();
 }
+*/
+
+function formDataToJSON(element) {
+    const formData = new FormData(element),
+    convertedJSON = {};
+
+    formData.forEach(function (value, key) {
+        convertedJSON[key] = value;
+    });
+
+    return convertedJSON;
+}
+
+document.querySelector('#add-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const formElement = document.forms['add'];
+    const json = formDataToJSON(formElement);
+    json.date = new Date();
+    console.log(json);
+
+});
