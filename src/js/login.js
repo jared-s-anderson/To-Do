b72c9b6d1311f7b6e4c4496d0e8e154dea8caf6a
Mitchell Salyards <sal17030@byui.edu>
2022-03-29 11:48:42 -0600

    Mock server to run api endpoints.

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,7 @@
+import ExternalServices from "./externalServices";
+
+const services = new ExternalServices();
+
 function formDataToJSON(formElement) {
     const formData = new FormData(formElement),
       convertedJSON = {};
@@ -9,15 +13,27 @@ function formDataToJSON(formElement) {
     return convertedJSON;
   }
 
+  async function login(user){
+    try {
+      const res = await services.login(user);
+      console.log(res);
+      // document.location = '../';
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
 
 
-document.querySelector('#loginbutton').addEventListener('click', (e) => {
+ document.querySelector('#loginbutton').addEventListener('click', (e) => {
     e.preventDefault();
     
 
     const formElement = document.forms['login'];
     const json = formDataToJSON(formElement);
     console.log(json)
+
+    login(json)
     
   });
+
